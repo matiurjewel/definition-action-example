e49a2e7276e08dbbff85befe16353580ab368a77
Matiur Rahman <matiur.rahman.web.developer@gmail.com>
2022-06-18 22:06:19 +0600

    useReducer hook is implemented

--- src/Hooks/UseReducer/index.jsx
+++ src/Hooks/UseReducer/index.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+
+    case "decrement":
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+
+    case "reset":
+      return {
+        ...state,
+        counter: 0,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  counter: 0,
+};
 
 const UseReducerHookExample = () => {
-  return <div>UseReducerHookExample</div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+      <br />
+      {state.counter}
+    </div>
+  );
 };
 
 export default UseReducerHookExample;
